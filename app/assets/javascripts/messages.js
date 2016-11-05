cfe4c0a20ed292aae718138068406e46eb04b724
Spencer Oberstadt <soberstadt@gmail.com>
2016-11-05 18:47:41 -0400

    improved reporting

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -5,7 +5,7 @@ $(function() {
         return $('#messages').prepend(this.renderMessage(data));
       },
       renderMessage: function(data) {
-        return "<li><div class='col s12 m2'><div class='z-depth-2 light-blue darken-3'><h3>" + data.app.name + ": </h3>" + data.content + "</div></div></li>";
+        return "<li><div class='col s12 m2'><div class='z-depth-2 light-blue darken-3'><h3>" + data.app.name + ": </h3>Device ID: " + data.content + "<h5>" + data.device_id + "</h5></div></div></li>";
       }
     });
   }
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,10 +4,13 @@ class MessagesController < ApplicationController
 
   def show
     # ActionCable.server.broadcast 'messages', data
+    app = Application.find_by(app_id: params[:app_id])
+    render(plain: 'App not found') and return unless app
     ActionCable.server.broadcast 'messages',
                                  {
-                                   content: params[:id],
-                                   app: { name: "JF" }
+                                   content: params[:content],
+                                   app: { name: app.title },
+                                   device_id: params[:device_id]
                                  }
 
     render plain: 'OK'
