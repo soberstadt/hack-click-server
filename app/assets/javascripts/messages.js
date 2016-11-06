b8fd40771cdce64510bc04463bd40f27e0213fb8
Spencer Oberstadt <soberstadt@gmail.com>
2016-11-06 10:26:49 -0500

    fix live feed listing

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,11 +1,15 @@
 $(function() {
   if($('#messages')[0]) {
+    var darkness = 6;
     App.snippets = App.cable.subscriptions.create('MessagesChannel', {
       received: function(data) {
         return $('#messages').prepend(this.renderMessage(data));
       },
       renderMessage: function(data) {
-        return "<li><div class='col s12 m2'><div class='z-depth-2 light-blue darken-3'><h3>" + data.app.name + ": </h3>Device ID: " + data.content + "<h5>" + data.device_id + "</h5></div></div></li>";
+        darkness -= 1;
+        if(darkness == 2)
+          darkness = 5;
+        return "<li><div class='col s12 m2'><div class='z-depth-2 light-blue darken-" + darkness + "'><h3>" + data.app.name + ": </h3>" + data.content + "<h5>Device ID: " + data.device_id + "</h5></div></div></li>";
       }
     });
   }
